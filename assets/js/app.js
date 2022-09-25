557b4579a9cc933d9d3ae0259501ec2aed8c2771
CarlDesigner <krlitosfajardo626@gmail.com>
2022-09-25 03:59:40 -0500

    Se crea la iteracion de los productos consumiendo el json, falta pulir las tarjetas para que salgan en linea

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,51 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    const productContainer = document.querySelector('#productContainer');
+    const shuffle = (array) => {
+        array.sort(() => Math.random() - 0.5);
+    }
+
+    fetch('/assets/data/product.json')
+        .then(response => response.json())
+        .then(data => {
+            shuffle(data);
+            data.forEach(product => {
+                productContainer.innerHTML += `
+            <div class="container">
+                <div class="row-cols-md-3 py-5">
+                    <div class="col">
+                        <div class="card shadow-sm py-2">
+                            <img src="/assets/img/products/${product.id}.png" class="img-product py-3" alt="">
+                
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                    <a type="button" class="btn btn-success btn-lg"><i class="bi bi-whatsapp"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+            })
+
+            console.log(data);
+        })
+})
+
+
+
+
+
+
+
+
+
+
 /* slider */
-$(document).ready(function(){
+$(document).ready(function () {
     $('.customer-logos').slick({
         slidesToShow: 6,
         slidesToScroll: 1,
@@ -7,11 +53,11 @@ $(document).ready(function(){
         autoplaySpeed: 1500,
         arrows: false,
         dots: false,
-        pauseOnHover:false,
+        pauseOnHover: false,
         responsive: [{
             breakpoint: 768,
             setting: {
-                slidesToShow:4
+                slidesToShow: 4
             }
         }, {
             breakpoint: 520,
