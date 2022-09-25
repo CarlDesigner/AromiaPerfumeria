a47ceb128a7c6dfd9dede996bb9aef6f4ad543bd
CarlDesigner <krlitosfajardo626@gmail.com>
2022-09-24 23:37:24 -0500

    Se agrega slider top brands

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+/* slider */
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover:false,
+        responsive: [{
+            breakpoint: 768,
+            setting: {
+                slidesToShow:4
+            }
+        }, {
+            breakpoint: 520,
+            setting: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
